0a455e4b2c2d6a19cca578b5a18fcec1a21c3608
Walter-Dingfelder <157520133+Walter-Dingfelder@users.noreply.github.com>
2026-03-23 20:40:19 -0400

    Add build and esbuild configurations to Vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Ensures React Router works on Netlify with client-side routing
   base: '/',
+  build: {
+    minify: 'esbuild',
+    target: 'es2015',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  esbuild: {
+    keepNames: true
+  }
 })
